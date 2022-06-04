73f785fa315ba84805e0de621dcf85e5d468585a
umayanga1997 <umayanga.avi@gmail.com>
2022-06-04 13:21:34 +0530

    ssr off

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  ssr: true,
+  ssr: false,
 
   head: {
     titleTemplate: "%s - TLA",
